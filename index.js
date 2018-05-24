b6d073ace85df462d567ab16ba4465e7bbfe16ac
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-05-24 14:58:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+class Driver {
+  constructor(name, startDateAsString) {
+    this.name = name;
+    this.startDateAsString = startDateAsString;
+  }
+  
+  startDate () {
+    const date =  new Date(startDateAsString);
+    this.startDate = date;
+  }
+}
