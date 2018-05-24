0359d6b343397ce1549bf3417d4507efdf355a77
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-05-24 15:25:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
 
+class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  
+  blocksTravelled () {
+    const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    
+    const verticalTravelled = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    
+    const horizontalTravelled = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    
+    return verticalTravelled + horizontalTravelled;
+  }
+  
+  estimatedTime (isPeakHours) {
+    if (isPeakHours) {
+      return this.blocksTravelled() / 2;
+    }
+    
+    else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+    
+    
+}
